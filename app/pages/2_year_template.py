fed326f4ee38461e7c3a119f566b49d9ddea0868
Gustavo Santos <gupaulasan@gmail.com>
2025-02-03 13:07:26 -0300

    feat: Add top tracks and year selection
    Change from template to year selection for better use

--- app/pages/2_year_template.py
+++ app/pages/2_year_template.py
@@ -21,17 +21,18 @@ def format_time(milliseconds):
 sqlite3.register_converter("DATETIME", convert_date)
 
 # Config
-YEAR = 2022
-first_of_the_year = datetime.date(YEAR, 1, 1)
-last_of_the_year = datetime.date(YEAR, 12, 31)
-
 st.set_page_config(
     page_title="Spotify Super Wrapped",
     page_icon=":material/play_circle:",
     layout="wide",
 )
 
-st.title(f"{YEAR} Spotify Wrapped")
+year_select, _ = st.columns([1, 2])
+YEAR = year_select.selectbox("Select the year you want to analyse", range(2015, 2025))
+first_of_the_year = datetime.date(YEAR, 1, 1)
+last_of_the_year = datetime.date(YEAR, 12, 31)
+
+st.title(f"{YEAR} Stats")
 
 st.write(
     f"This page presents a detailed analysis of your Spotify data for the year {YEAR}."
@@ -76,10 +77,24 @@ group_by_album = (
 )
 
 top_albums_text = "\n".join(
-    f"{i + 1}. **{album}**: {format_time(group_by_album[i])}, across {top_10_albums.loc[album]} plays"
+    f"{i + 1}. **{album} ({df.loc[lambda df: df['album_name'] == album].iloc[0]['artist_name']})**: {format_time(group_by_album[i])}, across {top_10_albums.loc[album]} plays"
     for i, album in enumerate(group_by_album.index[:10])
 )
 
 st.write(
     f"## Top 10 albums\n\nHere are your top 10 albums of {YEAR}\n\n{top_albums_text}"
+)
+
+top_10_tracks = df["track_name"].value_counts()
+group_by_track = (
+    df.groupby("track_name")["ms_played"].sum().sort_values(ascending=False)
+)
+
+top_tracks_text = "\n".join(
+    f"{i + 1}. **{track} ({df.loc[lambda df: df['track_name'] == track].iloc[0]['artist_name']})** {format_time(group_by_track[i])}, across {top_10_tracks.loc[track]} plays"
+    for i, track in enumerate(group_by_track.index[:10])
+)
+
+st.write(
+    f"## Top 10 tracks\n\nHere are yout top 10 tracks of {YEAR}\n\n{top_tracks_text}"
 )
