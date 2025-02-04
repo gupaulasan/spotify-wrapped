1c5df49d0942ee138266fff4e495b9584910ae7d
Gustavo Santos <gupaulasan@gmail.com>
2025-02-04 16:49:59 -0300

    feat: Download data from gcloud when opening the app

--- app/Home.py
+++ app/Home.py
@@ -1,4 +1,9 @@
+import os
+
 import streamlit as st
+from google.cloud import storage
+
+os.chdir("./")
 
 st.set_page_config(
     page_title="Spotify Super Wrapped",
@@ -6,6 +11,24 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="collapsed",
 )
+
+# Configurar credenciais (salve o JSON na pasta do projeto)
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "spotify-data-2710-572835308829.json"
+
+
+def download_db(bucket_name, source_blob_name, destination_file_name):
+    """Baixa o banco do Google Cloud Storage"""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
+
+# Baixando o banco
+BUCKET_NAME = "spotify-streamlit-app-db"
+DB_FILE = "my_spotify_data.db"
+
+download_db(BUCKET_NAME, DB_FILE, DB_FILE)
 
 st.write("# Spotify Super Wrapped")
 
